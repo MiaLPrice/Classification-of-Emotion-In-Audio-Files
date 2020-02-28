0b5169e1def64b1ac0feccc3b4468549c7f14c65
MiaLPrice <43227891+MiaLPrice@users.noreply.github.com>
2020-02-28 10:06:04 -0500

    i dont know what  i did

--- create_csv.py
+++ create_csv.py
@@ -11,7 +11,7 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 # The array that will be converted to a dataframe
 for_df = []
-
+for_emotion = []
 # Going though each files and spliting them at the "-" 
 # to make a file that will be used for surpervised learning
 for each in onlyfiles:
@@ -25,9 +25,19 @@ for each in onlyfiles:
         split_each.append(1)
     split_each.insert(0, each)
     for_df.append(split_each)
+    for_emotion.append([each,split_each[3]])
 
 # convert array to dataframe
 for_csv = pd.DataFrame(for_df, columns = ['file_name' , 'Modality', 'Voice_channel', 'Emotion', 'Intensity', 'Statement', 'Repetition', 'Actor', 'Gender']) 
 
 # write dataframe to csv
 for_csv.to_csv("./ravdess_audio_file_attributes.csv", sep=',',index=False)
+
+
+emotions = pd.DataFrame(for_emotion, columns = ['file_name', 'Emotion'])
+# 'neutral' , 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprised'
+
+x = pd.get_dummies(emotions['Emotion'])
+emotions  = pd.concat([emotions, x], axis=1, join='inner')
+emotions = emotions.drop(columns = ['Emotion'])
+emotions.to_csv("./emotion.csv", sep=',',index=False)
